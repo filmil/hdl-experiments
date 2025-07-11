0941e2b230366e836ff7d14f9b2d4114fef275a4
Filip Filmar <filmil@gmail.com>
2025-07-11 03:10:12 +0000

    feat: experiment one

--- sv/dff.sv
+++ sv/dff.sv
@@ -1,7 +1,18 @@
+package dffx;
+
+    //interface one_ff (
+        //input logic clk,
+        //input logic reset_n
+    //);
+    //endinterface
+
+    parameter dff_bits_count = 8;
+endpackage
+
+// Test
 module dff #(
-    BITS_COUNT = 7
-)
-(
+    BITS_COUNT = 8
+)(
     input logic clk,
     input logic [BITS_COUNT-1:0] d,
     input logic rst_n,
@@ -11,12 +22,13 @@ module dff #(
 
     logic [BITS_COUNT-1:0] d_s;
 
+    // The sequential process bits.
     always_ff @(posedge clk) begin
-        // Synchronous reset here.
         q <= d_s;
-        q_n <= ~d_s;
+        q_n <= ~d_s; // d_s == d (as signal)
     end
 
+    // The combinatorial process bits.
     always_comb begin
         d_s = d;
         if (~rst_n) begin
---
+++ sv/top.sv
@@ -0,0 +1,31 @@
+// Presumably includes are no longer needed.
+`include "dff.svh"
+
+// Here's how to get to a constant from a package.
+import dffx::dff_bits_count;
+
+module top(
+    input logic clk,
+    input logic reset_n,
+
+    // This is imported from the module above.
+    input logic [dff_bits_count-1:0] input_data,
+
+    // No need to import if referring to full library.
+    output logic [dffx::dff_bits_count-1:0] output_data
+    );
+
+    logic [7:0] internal_data;
+
+    dff #(
+        .BITS_COUNT(dff_bits_count)
+    ) dff_i (
+        .clk(clk),
+        .rst_n(reset_n),
+        .d(input_data),
+        .q(output_data)
+    );
+
+    assign output_data = internal_data;
+
+endmodule

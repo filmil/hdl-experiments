2932b34878fbc2d028bbf140f7a5654b8f86b924
Filip Filmar <filmil@gmail.com>
2026-01-16 02:17:27 -0800

    feat: update the library and vhdl_ls.toml

--- src/ex4/lib1/lib1.vhdl
+++ src/ex4/lib1/lib1.vhdl
@@ -3,10 +3,12 @@
 --! @package lib1
 --! @brief A library package containing a simple constant.
 package lib1 is
-    
+
    -- This is ex4_lib1.lib1.c
    --! @constant c
    --! @brief An integer constant initialized to 1.
-   constant c: integer := 1; 
-    
+   constant c: integer := 1;
+
+   constant d: integer := c + 2;
+
 end package lib1;
